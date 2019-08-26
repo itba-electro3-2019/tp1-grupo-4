88a75979bc4c1dc4af8f0e9f13d3f9d4fcb1cd52
JuanMartinLaguinge <jlaguinge@itba.edu.ar>
2019-08-26 15:34:38 -0300

    Test bash

---
+++ Ejercicio_4/Prueba.sh
@@ -0,0 +1,17 @@
+echo Tabla de Verdad
+vvp ./execute/Proj +x=0000 rm ./execute/Proj
+vvp ./execute/Proj +x=0001 rm ./execute/Proj
+vvp ./execute/Proj +x=0010 rm ./execute/Proj
+vvp ./execute/Proj +x=0011 rm ./execute/Proj
+vvp ./execute/Proj +x=0100 rm ./execute/Proj
+vvp ./execute/Proj +x=0101 rm ./execute/Proj
+vvp ./execute/Proj +x=0110 rm ./execute/Proj
+vvp ./execute/Proj +x=0111 rm ./execute/Proj
+vvp ./execute/Proj +x=1000 rm ./execute/Proj
+vvp ./execute/Proj +x=1001 rm ./execute/Proj
+vvp ./execute/Proj +x=1010 rm ./execute/Proj
+vvp ./execute/Proj +x=1011 rm ./execute/Proj
+vvp ./execute/Proj +x=1100 rm ./execute/Proj
+vvp ./execute/Proj +x=1101 rm ./execute/Proj
+vvp ./execute/Proj +x=1110 rm ./execute/Proj
+vvp ./execute/Proj +x=1111 rm ./execute/Proj
